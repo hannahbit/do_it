00654738a758f5f8ba18f535f87965302cac90e4
Hannah Voget <hannah@bitcrowd.net>
2021-06-08 16:00:39 +0200

    /api/list/:id returns dummy list

--- lib/do_it_web/controllers/list_controller.ex
+++ lib/do_it_web/controllers/list_controller.ex
@@ -1,10 +1,15 @@
 defmodule DoItWeb.ListController do
   use DoItWeb, :controller
 
+  def show(conn, _params) do
+    list = %{title: "Foo"}
+
+    render(conn, "show.json", list: list)
+  end
+
   def create(conn, _params) do
     list = %{title: "test"}
 
     render(conn, "show.json", list: list)
   end
-
 end
