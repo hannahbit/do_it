4b1322c63e497a2d87a1cf41c34feff42a23bdbe
Hannah Voget <hannah@bitcrowd.net>
2021-06-09 17:57:57 +0200

    Add tests for ListController#show

--- test/do_it_web/controllers/list_controller_test.exs
+++ test/do_it_web/controllers/list_controller_test.exs
@@ -1,5 +1,22 @@
 defmodule DoItWeb.ListControllerTest do
   use DoItWeb.ConnCase
+  alias DoIt.List
+  alias DoIt.Repo
+
+  describe "show" do
+    test "renders list when list with that id exists", %{conn: conn} do
+      {:ok, list} = Repo.insert(%List{title: "Hello"})
+      conn = get(conn, Routes.list_path(conn, :show, list.id))
+      assert %{"title" => "Hello"} == json_response(conn, 200)["data"]
+    end
+
+    test "renders 404 error when list with that id does not exists", %{conn: conn} do
+      {:ok, list} = Repo.insert(%List{title: "Huhu"})
+      Repo.delete(list)
+      conn = get(conn, Routes.list_path(conn, :show, list.id))
+      assert "Not Found" == json_response(conn, 404)
+    end
+  end
 
   describe "create" do
     test "renders list when data is valid", %{conn: conn} do
