64b1fe460fb10f9adf5762f14580ec5db03ee3ab
Hannah Voget <hannah@bitcrowd.net>
2021-06-09 15:19:55 +0200

    Insert new list to repo, show error message otherwise

--- lib/do_it_web/views/list_view.ex
+++ lib/do_it_web/views/list_view.ex
@@ -8,4 +8,15 @@ defmodule DoItWeb.ListView do
   def render("list.json", %{list: list}) do
     %{title: list.title}
   end
+
+  def render("error.json", %{changeset: changeset}) do
+
+    errors = Ecto.Changeset.traverse_errors(changeset, fn {msg, opts} ->
+      Enum.reduce(opts, msg, fn {key, value}, acc ->
+        String.replace(acc, "%{#{key}}", to_string(value))
+      end)
+    end)
+
+    %{errors: errors}
+  end
 end
