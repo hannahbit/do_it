7dac1f274eaea763d290b74f887cd459dab82509
Hannah Voget <hannah@bitcrowd.net>
2021-06-09 16:14:42 +0200

    Add tests for ListController#create

---
+++ test/do_it_web/controllers/list_controller_test.exs
@@ -0,0 +1,15 @@
+defmodule DoItWeb.ListControllerTest do
+  use DoItWeb.ConnCase
+
+  describe "create" do
+    test "renders list when data is valid", %{conn: conn} do
+      conn = post(conn, Routes.list_path(conn, :create), title: "Good Title")
+      assert %{"title" => "Good Title"} == json_response(conn, 200)["data"]
+    end
+
+    test "renders error when data is invalid", %{conn: conn} do
+      conn = post(conn, Routes.list_path(conn, :create), title: "Go")
+      assert %{"title" => ["should be at least 3 character(s)"]} = json_response(conn, 400)["errors"]
+    end
+  end
+end
