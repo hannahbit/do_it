c509dc709156c231349ce179132a94c49b060201
Hannah Voget <hannah@bitcrowd.net>
2021-06-09 16:46:59 +0200

    Just saving before switching the branch

--- lib/do_it_web/controllers/list_controller.ex
+++ lib/do_it_web/controllers/list_controller.ex
@@ -1,11 +1,16 @@
 defmodule DoItWeb.ListController do
   use DoItWeb, :controller
 
-  def show(conn, _params) do
-    list = %{title: "Foo"}
-
-    render(conn, "show.json", list: list)
-  end
+  # def show(conn, %{"id" => id}) do
+  #   case Ecto.Repo.get(List, id) do
+  #     {:ok, list}         -> conn
+  #                             |> put_status(200)
+  #                             |> render("show.json", list: list)
+  #     {:error, changeset} -> conn
+  #                             |> put_status(400)
+  #                             |> render("error.json", changeset: changeset)
+  #   end
+  # end
 
   def create(conn, _params) do
     list = %{title: "test"}
