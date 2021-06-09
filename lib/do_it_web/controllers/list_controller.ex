303d679745a809b4272cdc14c1ad616beca11a50
Hannah Voget <hannah@bitcrowd.net>
2021-06-09 17:34:29 +0200

    api/list/:id shows list or error

--- lib/do_it_web/controllers/list_controller.ex
+++ lib/do_it_web/controllers/list_controller.ex
@@ -3,16 +3,20 @@ defmodule DoItWeb.ListController do
   alias DoIt.List
   alias DoIt.Repo
 
-  # def show(conn, %{"id" => id}) do
-  #   case Ecto.Repo.get(List, id) do
-  #     {:ok, list}         -> conn
-  #                             |> put_status(200)
-  #                             |> render("show.json", list: list)
-  #     {:error, changeset} -> conn
-  #                             |> put_status(400)
-  #                             |> render("error.json", changeset: changeset)
-  #   end
-  # end
+  def show(conn, %{"id" => id}) do
+    case Repo.get(List, id) do
+      list = %List{} ->
+        conn
+        |> put_status(200)
+        |> render("show.json", list: list)
+
+      nil ->
+        conn
+        |> put_status(404)
+        |> put_view(DoItWeb.ErrorView)
+        |> render("404.json")
+    end
+  end
 
   def create(conn, params) do
     result =
