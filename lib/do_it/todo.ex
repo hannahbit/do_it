8d7854715455e15acda0cc1388a5a34f853818d3
Hannah Voget <hannah@bitcrowd.net>
2021-06-11 12:31:40 +0200

    Save todos only when associated to a list

--- lib/do_it/todo.ex
+++ lib/do_it/todo.ex
@@ -3,19 +3,19 @@ defmodule DoIt.Todo do
   import Ecto.Changeset
 
   schema "todos" do
-    belongs_to :lists, DoIt.List
+    belongs_to :list, DoIt.List
     field :checked, :boolean, default: false
     field :description, :string
-    field :list_id, :id
 
     timestamps()
   end
 
   @doc false
-  def changeset(todo, attrs) do
+  def create_changeset(todo, attrs) do
     todo
     |> cast(attrs, [:description, :checked])
     |> validate_required([:description, :list_id])
     |> validate_length(:description, min: 3, max: 100)
+    |> assoc_constraint(:list)
   end
 end
