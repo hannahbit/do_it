2ab6db41176e09f8f2c005506fd7929929d1cf5d
Hannah Voget <hannah@bitcrowd.net>
2021-06-15 12:42:13 +0200

    Handle unexisting list error in Repo
    This moves the handling of unexisting list errors into the repo
    instead of leaving it in the fallback controller.
    Like this less internal knowledge is spread into other places of the app.

--- lib/do_it/repo.ex
+++ lib/do_it/repo.ex
@@ -5,6 +5,10 @@ defmodule DoIt.Repo do
 
   alias DoIt.{List, Todo}
 
+  @list_does_not_exist [
+    list: {"does not exist", [constraint: :assoc, constraint_name: "todos_list_id_fkey"]}
+  ]
+
   def get_list(id) do
     case get(List, id) do
       nil -> {:error, :not_found}
@@ -25,5 +29,15 @@ defmodule DoIt.Repo do
     params
     |> Todo.create_changeset()
     |> insert()
+    |> handle_unexisting_list_error()
+  end
+
+  defp handle_unexisting_list_error(insert_return) do
+    {status, result} = insert_return
+    if Map.has_key?(result, :errors) && result.errors == @list_does_not_exist do
+      {:error, :not_found}
+    else
+      {status, result}
+    end
   end
 end
