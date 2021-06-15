e013073d9f80ce7818e83f0536f9e6c784c5dd18
Hannah Voget <hannah@bitcrowd.net>
2021-06-15 11:58:56 +0200

    Use conn from the setup block

--- test/do_it_web/controllers/todo_controller_test.exs
+++ test/do_it_web/controllers/todo_controller_test.exs
@@ -5,23 +5,20 @@ defmodule DoItWeb.TodoControllerTest do
   @invalid_description %{description: ""}
 
   describe "create" do
-    test "renders 404 when list does not exist" do
-      conn = build_conn()
+    test "renders 404 when list does not exist", %{conn: conn} do
       path = Routes.list_todo_path(conn, :create, 461_212)
       conn = post(conn, path, @valid_description)
       assert response(conn, 404)
     end
 
-    test "renders 400 if invalid data given" do
-      conn = build_conn()
+    test "renders 400 if invalid data given", %{conn: conn} do
       list = insert(:list)
       path = Routes.list_todo_path(conn, :create, list.id)
       conn = post(conn, path, @invalid_description)
       assert response(conn, 400)
     end
 
-    test "saves todo and redirects to ListController#show when data valid and list exists" do
-      conn = build_conn()
+    test "saves todo and redirects to ListController#show when data valid and list exists", %{conn: conn} do
       list = insert(:list)
       path = Routes.list_todo_path(conn, :create, list.id)
       conn = post(conn, path, @valid_description)
